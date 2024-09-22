1856fbaa5c19ad3939dbd0c0d510d65b0ef63b18
waleedabass <137681428+waleedabass@users.noreply.github.com>
2024-09-22 23:51:47 +0500

    Made Website Better and added JS Object.

---
+++ script.js
@@ -0,0 +1,31 @@
+const profileData = {
+    name: "Waleed Abbas",
+    summary: "I am a fifth-semester Computer Science student at the Information Technology University, Pakistan. I specialize in front-end website development with skills in React JS, WordPress, HTML, CSS, JavaScript, MySQL, and Node JS. I also have experience in Python Machine Learning. Certified in prompt engineering and AI utilization, I have a strong understanding of DSA and OOP, and I am confident in my ability to contribute effectively to your firm.",
+    skills: {
+        softSkills: ["Leadership", "Communication", "Time Management", "Cooperative", "Adapt quickly", "Critical Thinking"],
+        hardSkills: ["HTML/CSS/JavaScript", "Laravel", "React JS", "Python", "Machine Learning", "OOP", "Data Structures & Algorithms", "MySQL"]
+    },
+    projects: [
+        "Project 1 - Jan 2023: Developed an e-commerce website.",
+        "Project 2 - Developed a Professional Vape selling Website as a side project.",
+        "Project 3 - Made a Jarvis-like Voice Assistant for PC.",
+        "Project 4 - Wrote a Python Script to synchronize folders using OS Module.",
+        "Project 5 - Made a Pdf Merger using Python."
+    ],
+    certifications: [
+        "Python Crash Course",
+        "Web Scraping",
+        "GPT Advanced Data Analysis",
+        "WordPress",
+        "Machine Learning",
+        "Front-end Development",
+        "Canva",
+        "Prompt Engineering",
+        "Participated in GDSC Hackathon"
+    ],
+    education: [
+        "Matriculation - The Punjab School (2009-2019)",
+        "Intermediate - KIPS College (2019-2021)",
+        "Bachelor's in Computer Science - ITU (2022-2026)"
+    ]
+};
